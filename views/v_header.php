75ac79f55e04deb31a38fb07c5305392c71b0e0a
MarieRmsy <ramssamy.marie@gmail.com>
2022-03-31 10:34:08 +0200

    up

---
+++ views/v_header.php
@@ -0,0 +1,36 @@
+<div id="header-wrapper">
+					<header id="header" class="container">
+
+						<!-- Logo -->
+							<div id="logo">
+								<h1><a href="index.html">Virtuoz</a></h1>
+								<span>Conservatoire de musique nouvelle génération</span>
+							</div>
+
+						<!-- Nav -->
+							<nav id="nav">
+								<ul>
+									<li class="current"><a href="index.html">Accueil</a></li>
+									<li>
+										<a href="index.php?action=course">Cours</a>
+										<ul>
+											<li><a href="#">Piano</a></li>
+											<li><a href="#">Guitare</a></li>
+											<li>
+												<a href="#">Trompette</a>
+												<ul>
+													<li><a href="#">Lorem ipsum dolor</a></li>
+													<li><a href="#">Phasellus consequat</a></li>
+													<li><a href="#">Magna phasellus</a></li>
+													<li><a href="#">Etiam dolore nisl</a></li>
+												</ul>
+											</li>
+										</ul>
+									</li>
+									<li><a href="index.php?action=inscription">Inscription</a></li>
+									<!-- <li><a href="right-sidebar.html">Right Sidebar</a></li> -->
+								</ul>
+							</nav>
+
+					</header>
+</div>
--- views/v_inscription.php
+++ views/v_inscription.php
@@ -1,21 +1,5 @@
-<!DOCTYPE HTML>
-<!--
-	Verti by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
-<html>
-	<head>
-		<title>Verti by HTML5 UP</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-	</head>
-	<body class="is-preload no-sidebar">
-		<div id="page-wrapper">
-
 			<!-- Main -->
-				<div id="main-wrapper">
+            <div id="main-wrapper">
 					<div class="container">
 						<div id="content">
 
@@ -51,17 +35,3 @@
 						</div>
 					</div>
 				</div>
-
-			</div>
-
-		<!-- Scripts -->
-
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.dropotron.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-
-	</body>
-</html>
